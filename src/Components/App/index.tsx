decd9a6ffcb4443e16173856d575f062842380b4
Alejandro <consiglialejandro@gmail.com>
2023-05-17 18:14:18 +0200

    Adding component logic and interfaces

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,10 +1,42 @@
+import { useState, useEffect } from "react";
 import "./styles.css";
 import Autocomplete from "../Autocomplete";
 
+const mockedFruits = ["Banana", "Orange", "Apple"];
+
 const App = () => {
+  const [search, setSearch] = useState<string>("");
+  const [options, setOptions] = useState<string[]>([]);
+
+  // creating a filtering async function
+  const filterFruits = async (fruits: string[]): Promise<string[]> => {
+    return new Promise((resolve) => {
+      const filteredFruits = fruits.filter((fruit: string) =>
+        fruit.toLowerCase().includes(search.toLowerCase())
+      );
+      resolve(filteredFruits);
+    });
+  };
+
+  //fetching data from mocked data
+  const fetchData = async () => {
+    try {
+      const fruits = await filterFruits(mockedFruits);
+      setOptions(fruits);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [search]);
+
   return (
     <div className="app">
-      <Autocomplete />
+      <h1 className="title">User Database</h1>
+      <h2 className="subtitle">Please search for a user in our Database</h2>
+      <Autocomplete search={search} options={options} setSearch={setSearch} />
     </div>
   );
 };
--- src/Components/Autocomplete/index.tsx
+++ src/Components/Autocomplete/index.tsx
@@ -1,7 +1,39 @@
+import { FunctionComponent, ChangeEvent } from "react";
 import "./styles.css";
 
-const Autocomplete = () => {
-  return <div className="autocomplete">Autocomplete</div>;
+interface Props {
+  search: string;
+  options: string[];
+  setSearch: (search: string) => void;
+}
+
+const Autocomplete: FunctionComponent<Props> = ({ options, setSearch }) => {
+  // updating search input
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearch(value);
+  };
+
+  return (
+    <div className="autocomplete">
+      <input
+        type="text"
+        name=""
+        placeholder="Search here"
+        onChange={handleInputChange}
+        className="input"
+      />
+      {options.length > 0 && (
+        <ul className="options">
+          {options.map((option, key) => (
+            <li key={key} className="option">
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Autocomplete;
