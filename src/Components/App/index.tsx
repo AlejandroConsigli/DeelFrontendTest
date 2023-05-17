69686bf4e9ebd333e5a9a27e150ea2138782e4aa
Alejandro <consiglialejandro@gmail.com>
2023-05-17 17:50:32 +0200

    Creating and organizing components

---
+++ src/Components/App/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Autocomplete from "../Autocomplete";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Autocomplete />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Autocomplete/index.tsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+const Autocomplete = () => {
+  return <div className="autocomplete">Autocomplete</div>;
+};
+
+export default Autocomplete;
